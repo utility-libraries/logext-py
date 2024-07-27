cf0ebb55b13845f33950ed0c43935b2740b5fba9
playerg9 <g9yout@gmail.com>
2024-07-27 13:17:48 +0200

    project setup

---
+++ src/loggext/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding=utf-8 -*-
+r"""
+
+"""
+
+__author__ = "PlayerG9"
+__copyright__ = "Copyright 2024, utility-libraries"
+__credits__ = ["PlayerG9"]
+__license__ = "MIT"
+__maintainer__ = "utility-libraries"
+__email__ = None
+__status__ = "Development"  # Prototype, Development, Production
+__description__ = "logging extensions for pythons logging library"
+__version_info__ = (0, 0, 0)
+__version__ = '.'.join(map(str, __version_info__))
