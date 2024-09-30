a5709a5cd439fb961ce2d6129ba6c2722e2cfa6f
playerg9 <g9yout@gmail.com>
2024-09-30 18:33:13 +0200

    extended utility with method to disable log-record attributes

--- src/loggext/utility/_impl.py
+++ src/loggext/utility/_impl.py
@@ -7,7 +7,10 @@ import sys
 import logging
 
 
-__all__ = ['logging_is_configured', 'is_running_as_service', 'is_running_in_shell']
+__all__ = [
+    'logging_is_configured', 'is_running_as_service', 'is_running_in_shell',
+    'no_thread_info', 'no_process_info', 'no_multiprocess_info',
+]
 
 
 def logging_is_configured() -> bool:
@@ -29,3 +32,18 @@ def is_running_in_shell() -> bool:
     :return: if the current script is running in a shell (or not)
     """
     return os.isatty(sys.stdin.fileno())
+
+
+def no_thread_info():
+    r""" disabled threading-information in the LogRecord for slight performance boost """
+    logging.logThreads = False
+
+
+def no_process_info():
+    r""" disabled process-information in the LogRecord for slight performance boost """
+    logging.logProcess = False
+
+
+def no_multiprocess_info():
+    r""" disabled multiprocessing-information in the LogRecord for slight performance boost """
+    logging.logMultiprocessing = False
